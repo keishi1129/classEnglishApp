584ca6a422e2c961ec9b3f6a04bff1d97bf3c3fa
yoshikawa keishi <a130809k@gmail.com>
2020-01-28 11:45:24 +0900

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,11 +24,11 @@ require.context('images', true, /\.(png|jpg|jpeg|svg)$/)
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-require("trix")
-require("@rails/actiontext")
+require("@rails/actiontext").start()
 require("@rails/ujs").start()
-require("turbolinks").start()
 require("@rails/activestorage").start()
+require("trix").start()
+require("turbolinks").start()
 
 // import "bootstrap"
 import 'slick-carousel'
