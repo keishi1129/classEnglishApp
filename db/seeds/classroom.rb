b26a28e0e61a860cf8f9c7bc75668a3ad661e337
yoshikawa keishi <a130809k@gmail.com>
2020-02-16 18:55:23 +0900

    update seed

--- db/seeds/classroom.rb
+++ db/seeds/classroom.rb
@@ -1,19 +1,19 @@
 teacher = Teacher.create!(
-  name: "huga",
-  email: "huga@gmail.com",
+  name: "山田太郎",
+  email: "huga@example.com",
   password: "password",
   password_confirmation: "password"
 )
 
 classroom = teacher.classrooms.create!(
-  name: '1年A組',
+  name: '1年1組',
   classroom_code: "1111"
 )
 
 30.times do |i|
   classroom.students.create!(
-    name: "hoge#{i + 1}",
-    email: "hoge#{i + 1}@example.com", 
+    name: "生徒#{i + 1}",
+    email: "st#{i + 1}@example.com", 
     password: "password",
     password_confirmation: "password",
   )
