70289a1bb422bb0badde01757dea53cd6f9e2900
yoshikawa keishi <a130809k@gmail.com>
2020-02-25 11:51:27 +0900

    setting s3

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,7 @@ set :repo_url,  'git@github.com:keishi1129/classEnglishApp.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.6.4' 
