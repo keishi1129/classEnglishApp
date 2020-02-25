430f01312946a8ad5c95c6a6342d1b939a40cc5e
yoshikawa keishi <a130809k@gmail.com>
2020-02-25 12:21:49 +0900

    credential

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,6 +4,17 @@ require 'carrierwave/storage/fog'
 
 
 CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
+    aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
+    region: 'ap-northeast-1'
+  }
+
+
+
   if Rails.env.development? || Rails.env.test? #開発とテストは今まで通りに
     config.storage = :file
   elsif Rails.env.production? #本番はS3に保存する
