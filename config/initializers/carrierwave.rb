0420b70df45738bce69755e610298c8cb7e53276
yoshikawa keishi <a130809k@gmail.com>
2020-02-25 12:01:13 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,15 +4,21 @@ require 'carrierwave/storage/fog'
 
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
-    aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-    region: 'ap-northeast-1'
-  }
-
-  config.fog_directory  = 'e-class-bucket'
+  if Rails.env.development? || Rails.env.test? #開発とテストは今まで通りに
+    config.storage = :file
+  elsif Rails.env.production? #本番はS3に保存する
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+# credentials下にaws_access_key_idとaws_secret_access_keyはあるよ
+      region: 'ap-northeast-1'
+    }
+    config.fog_directory  = 'e-class-bucket'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/e-class-bucket'
+  end
 end
+
+
